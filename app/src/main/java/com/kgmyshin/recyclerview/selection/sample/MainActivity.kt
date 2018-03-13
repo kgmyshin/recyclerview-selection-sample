511d164b176cae94319704dd0bd991397f272583
kgmyshin <kgmyshin82@gmail.com>
2018-03-13 19:27:25 +0900

    add show and clear menu

--- app/src/main/java/com/kgmyshin/recyclerview/selection/sample/MainActivity.kt
+++ app/src/main/java/com/kgmyshin/recyclerview/selection/sample/MainActivity.kt
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.selection.StorageStrategy
 import com.kgmyshin.recyclerview.selection.sample.databinding.ActivityMainBinding
@@ -50,6 +53,9 @@ class MainActivity : AppCompatActivity() {
                     selectionTracker?.isSelected(item) ?: false
         }
 
+        if (savedInstanceState != null) {
+            selectionTracker?.onRestoreInstanceState(savedInstanceState)
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
@@ -59,10 +65,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
-        if (savedInstanceState != null) {
-            selectionTracker?.onRestoreInstanceState(savedInstanceState)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(
+                R.menu.menus_main,
+                menu
+        )
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val itemId = item?.itemId ?: return super.onOptionsItemSelected(item)
+
+        when (itemId) {
+            R.id.menu_show -> {
+                val selectionText = selectionTracker?.let {
+                    it.selection.map {
+                        val target = it as Item // bug: selection dont have type parameter
+                        target.title
+                    }.joinToString("\n")
+                }
+                Toast.makeText(
+                        this,
+                        selectionText,
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.menu_clear -> {
+                selectionTracker?.clearSelection()
+            }
         }
+        return true
     }
 }
