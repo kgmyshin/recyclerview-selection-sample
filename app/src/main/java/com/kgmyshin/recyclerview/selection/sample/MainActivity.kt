e06c2a203beafe7838490b4aeba3d54db37b7225
kgmyshin <kgmyshin82@gmail.com>
2018-03-16 10:31:42 +0900

    use workaround

--- app/src/main/java/com/kgmyshin/recyclerview/selection/sample/MainActivity.kt
+++ app/src/main/java/com/kgmyshin/recyclerview/selection/sample/MainActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.selection.MutableSelection
 import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.selection.StorageStrategy
 import com.kgmyshin.recyclerview.selection.sample.databinding.ActivityMainBinding
@@ -79,9 +80,14 @@ class MainActivity : AppCompatActivity() {
         when (itemId) {
             R.id.menu_show -> {
                 val selectionText = selectionTracker?.let {
-                    it.selection.map {
-                        // bug?: selection dont have type parameter
-                        // https://issuetracker.google.com/issues/74514352
+                    // bug?: selection dont have type parameter
+                    // https://issuetracker.google.com/issues/74514352
+
+                    // workaround
+                    val copy = MutableSelection<Book>()
+                    it.copySelection(copy)
+
+                    copy.map {
                         val target = it as Book
                         target.title
                     }.joinToString("\n")
